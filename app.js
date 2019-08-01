049dd86b45f7d7d0f28d66291e33362221508f0e
r.james <rjimmy2512@gmail.com>
2019-07-31 21:34:33 -0700

    edited

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var greeting = prompt('Welcome to Our Website. Want to know more about God?');
-var answer;
+var hourNow = prompt('what is the current hour?');
+var greeting;
 
-if (greeting = 'Yes') {
-    answer = 'Great. You are on the right place. Please go ahead';
-} else if (greeting = 'No') {
-    answer = 'It is fine. Nice to meet you. Did you know that God is always good? Please go ahed to know more about Him.';
+if (hourNow > 18) {
+    greeting = 'good evening';
+} else if (hourNow > 12) {
+    greeting = 'good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'good morning';
 } else {
-    salutation = 'Welcome to Our Website'
+    greeting = 'welcome to class'
 }
 
+alert(greeting + ' brian');
+
 // this will update the HTML
-document.write('<h2>' + salutation + '</h2>');
+document.write('<h2>' + greeting + '</h2>');

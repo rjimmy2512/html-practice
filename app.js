3727cb40f6b500491b6ba3f89fe8f586dd15c188
r.james <rjimmy2512@gmail.com>
2019-08-01 16:02:42 -0700

    jss

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var retVal = confirm('Welcome to Our Website. Do You Want To Continue?')
-if( retVal == true) {
-    document.write ('Thank You. Hope You Will Learn More About Knowing God!');
-    return true;
+function getConfirmation() {
+   var retVal = confirm('Welcome to Our Website. Do You Want To Continue?')
+     if( retVal == true) {
+        document.write ('Thank You. Hope You Will Learn More About Knowing God!');
+        return true;
+     }
+     else {
+         document.write ('Sorry To See You Go!');
+         return false;
+     }
+
 }
-else {
-    document.write ('Sorry To See You Go!');
-    return false;
-}
+
 
 var el_up = document.getElementById("GFG_UP"); 
 var el_down = document.getElementById("GFG_DOWN"); 

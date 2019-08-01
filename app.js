3a11a587802ef2a175b4de04a52becbd1220f82b
r.james <rjimmy2512@gmail.com>
2019-08-01 15:09:09 -0700

    jss

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-var el_up = document.getElementById("GFG_UP"); 
-var el_down = document.getElementById("GFG_DOWN"); 
-var str = "Click on button to change the background color"; 
-
-el_up.innerHTML = str; 
-
-function changeColor(color) { 
-    document.body.style.background = color; 
-} 
-  
-function gfg_Run() { 
-    changeColor('pink'); 
-    el_down.innerHTML = "Background Color changed"; 
-}        
-
-
 var hourNow = prompt('what is the current hour?');
 var greeting;
 
@@ -31,3 +15,23 @@ alert(greeting + ' brian');
 
 // this will update the HTML
 document.write('<h2>' + greeting + '</h2>');
+
+
+
+
+var el_up = document.getElementById("GFG_UP"); 
+var el_down = document.getElementById("GFG_DOWN"); 
+var str = "Click on button to change the background color"; 
+
+el_up.innerHTML = str; 
+
+function changeColor(color) { 
+    document.body.style.background = color; 
+} 
+  
+function gfg_Run() { 
+    changeColor('pink'); 
+    el_down.innerHTML = "Background Color changed"; 
+}        
+
+

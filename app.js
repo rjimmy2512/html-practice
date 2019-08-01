d1a683963b13b1354c488f2c3082ccf15fef5e62
r.james <rjimmy2512@gmail.com>
2019-08-01 15:04:06 -0700

    jss edited

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-if (window.confirm("Welcome to Our Website. Do you want to know more about God?")) { 
-    window.open("exit.html", "Thanks for Visiting!");
-}
-
-
 var el_up = document.getElementById("GFG_UP"); 
 var el_down = document.getElementById("GFG_DOWN"); 
 var str = "Click on button to change the background color"; 
@@ -18,4 +13,20 @@ function gfg_Run() {
     el_down.innerHTML = "Background Color changed"; 
 }        
 
+var hourNow = prompt('what is the current hour?');
+var greeting;
 
+if (hourNow > 18) {
+    greeting = 'good evening';
+} else if (hourNow > 12) {
+    greeting = 'good afternoon';
+} else if (hourNow > 0) {
+    greeting = 'good morning';
+} else {
+    greeting = 'welcome to class'
+}
+
+alert(greeting + ' brian');
+
+// this will update the HTML
+document.write('<h2>' + greeting + '</h2>');

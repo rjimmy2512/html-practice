8de5ab34bd97b81bc3fdec7f7f3a064175c55a4c
r.james <rjimmy2512@gmail.com>
2019-07-31 22:12:21 -0700

    js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var hourNow = prompt('Do you want to know God? Yes/No');
-var greeting;
-
-if (hourNow = 'Yes') {
-    greeting = 'Welcome to Our Website';
-} else if (hourNow = 'No') {
-    greeting = 'Welcome. Maybe it is your time to know God!';
-} 
-
-alert(greeting + ' Please go ahead and know more.');
-
-// this will update the HTML
-document.write('<h2>' + greeting + '</h2>');
+if (window.confirm("Welcome to Our Website. Do you want to know more about God?")) { 
+    window.open("exit.html", "Thanks for Visiting!");
+  }
